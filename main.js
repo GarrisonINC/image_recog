4083b94e2aa062ffdc46fd8ff457d653174c65d4
GarrisonINC <111631791+GarrisonINC@users.noreply.github.com>
2023-01-23 15:09:28 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 100
+
+})
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = "<img id='captured_image' src='" + data_uri + "'/>"
+    })
+}
+
+console.log("ml5 vs: ",ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/OBCHPP5qU/model.json",modelLoaded);
+
+function modelLoaded (){
+    console.log("Model is loaded.");
+}
+ 
+
+function check(){
+img=document.getElementById('captured_image');
+classifier.classify(img,gotResult)
+}
+
+function gotResult(error,results){
+ if (error){
+    console.error(error);
+ } 
+ else{
+    console.log(results);
+    document.getElementById('object').innerHTML=results[0].label;
+    document.getElementById('confidence').innerHTML=(results[0].confidence)*100+'%';
+ } 
+}
